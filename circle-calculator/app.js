296592450048316909494a75ffaf764bb3723222
sabarish sekar <sabarish@bittiq.com>
2020-04-20 20:00:43 +0200

    adding new feature to support calcualting diameter and area

--- circle-calculator/app.js
+++ circle-calculator/app.js
@@ -2,12 +2,20 @@ let response;
 
 exports.lambdaHandler = async (event, context) => {
     const radius = event.queryStringParameters.radius;
-    const circleArea = Math.PI * (Math.pow(radius, 2) );
+    const type = event.queryStringParameters.type;
+    let value;
+    if (type === 'area') {
+        value = Math.PI * (Math.pow(radius, 2));
+    } else if (type === 'diameter') {
+        value = 2 * radius;
+    }
+    
     try {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                area: circleArea,
+                type: type,
+                result: value,
             })
         }
     } catch (err) {
--- circle-calculator/tests/unit/test-handler.js
+++ circle-calculator/tests/unit/test-handler.js
@@ -6,12 +6,14 @@ const expect = chai.expect;
 var event, context;
 
 describe('Tests index', function () {
-   event =  {
-        queryStringParameters : {
-            radius : "2"
-        }
-    };
-    it('verifies successful response', async () => {
+
+    it('verifies areas is calculated as expected', async () => {
+        event =  {
+            queryStringParameters : {
+                radius : '2',
+                type : 'area'
+            }
+        };
         const result = await app.lambdaHandler(event, context)
 
         expect(result).to.be.an('object');
@@ -21,6 +23,28 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.area).to.be.equal(12.566370614359172);
+        expect(response.result).to.be.equal(12.566370614359172);
+        expect(response.type).to.be.equal('area');
+    });
+
+    it('verifies diameter is calculated as expected', async () => {
+        event =  {
+            queryStringParameters : {
+                radius : '2',
+                type : 'diameter'
+            }
+        };
+        const result = await app.lambdaHandler(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+
+        expect(response).to.be.an('object');
+        expect(response.result).to.be.equal(4);
+        expect(response.type).to.be.equal('diameter');
+
     });
 });
